9ae08ad687353125b4c13ae09605490375ee2990
qmarkm <106965891+qmarkm@users.noreply.github.com>
2024-10-14 17:52:52 +0800

    Implemented color palette to colors.xml
    The standard colors for the project are put into colors.xml:
    
    Ocean Green
    Swampy Green
    Ocean Blue
    Lime Green
    Baby Green
    Shrek Green

--- app/src/main/java/com/mobdeve/s12/group10/mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group10/mco/MainActivity.kt
@@ -16,17 +16,7 @@ import com.mobdeve.s12.group10.mco.ui.theme.Group10MCOTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Group10MCOTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
     }
 }
 
