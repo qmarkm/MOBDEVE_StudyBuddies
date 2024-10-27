2204026d53fa3f8c5b7ea39558764d9283d82446
qmarkm <106965891+qmarkm@users.noreply.github.com>
2024-10-27 16:55:32 +0800

    Finished Update SP
    Added credits to certain parts as well

--- app/src/main/java/com/mobdeve/s12/group10/mco/MainStudyPacts.kt
+++ app/src/main/java/com/mobdeve/s12/group10/mco/MainStudyPacts.kt
@@ -6,6 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobdeve.s12.group10.mco.databinding.ActivityMainStudyPactsBinding
 
+/**
+ * Credits to:
+ * → https://www.youtube.com/watch?app=desktop&v=ROkKPgXpd1Y
+ * → https://github.com/android/platform-samples/blob/main/samples/user-interface/constraintlayout/src/main/res/xml/scene_02.xml
+ * → and Mr. Berris
+ *
+ * For the Motion Layout in activity_main_study_pacts.xml & activity_main_study_pacts_scene.xml
+ */
+
 class MainStudyPacts : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainStudyPactsBinding
 
--- app/src/main/java/com/mobdeve/s12/group10/mco/SPDetailed.kt
+++ app/src/main/java/com/mobdeve/s12/group10/mco/SPDetailed.kt
@@ -1,6 +1,12 @@
 package com.mobdeve.s12.group10.mco
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +15,17 @@ import com.mobdeve.s12.group10.mco.databinding.ActivitySpdetailedBinding
 import java.text.SimpleDateFormat
 import java.util.Locale
 
+/**
+ * Credits to: https://www.youtube.com/watch?v=TMQNvnU0_wE
+ *      For Dialog popup
+ */
+
 class SPDetailed : AppCompatActivity() {
     private lateinit var viewBinding: ActivitySpdetailedBinding
+    private lateinit var spTitle: String
+    private lateinit var spDesc: String
+    private lateinit var spDateTime: String
+    private lateinit var spLocation: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +33,24 @@ class SPDetailed : AppCompatActivity() {
         viewBinding = ActivitySpdetailedBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        viewBinding.txvTitle.text = intent.getStringExtra("SP_TITLE")
-        viewBinding.txvDesc.text = intent.getStringExtra("SP_DESCRIPTION")
-        viewBinding.txvDateField.text = intent.getStringExtra("SP_DATETIME")?.let { formatDate(it) }
-        viewBinding.txvTimeField.text = intent.getStringExtra("SP_DATETIME")?.let { formatTime(it) }
-        viewBinding.txvLocationField.text = intent.getStringExtra("SP_LOCATION")
+        spTitle = intent.getStringExtra("SP_TITLE").toString()
+        spDesc = intent.getStringExtra("SP_DESCRIPTION").toString()
+        spDateTime = intent.getStringExtra("SP_DATETIME").toString()
+        spLocation = intent.getStringExtra("SP_LOCATION").toString()
+
+
+        viewBinding.txvTitle.text = spTitle
+        viewBinding.txvDesc.text = spDesc
+        viewBinding.txvDateField.text = formatDate(spDateTime)
+        viewBinding.txvTimeField.text = formatTime(spDateTime)
+        viewBinding.txvLocationField.text = spLocation
 
         viewBinding.btnReturn.setOnClickListener {
             finish()
+        }
+
+        viewBinding.btnUpdate.setOnClickListener {
+            showUpdateDialog()
         }
     }
 
@@ -41,5 +66,32 @@ class SPDetailed : AppCompatActivity() {
         val readableFormat = SimpleDateFormat("hh:mm a", Locale.getDefault())
 
         return readableFormat.format(dbFormat.parse(time))
+    }
+
+    fun showUpdateDialog(){
+        val dialog = Dialog(this, R.style.DialogStyle)
+        dialog.setContentView(R.layout.dialog_spupdate)
+
+        dialog.getWindow()?.setBackgroundDrawableResource(R.drawable.button4)
+
+        val btnClose = dialog.findViewById<ImageButton>(R.id.btnClose)
+        btnClose.setOnClickListener{
+            dialog.dismiss()
+        }
+
+        val btnUpdate = dialog.findViewById<Button>(R.id.btnUpdate)
+        btnUpdate.setOnClickListener{
+            //TODO: Update parameters here
+            dialog.dismiss()
+        }
+
+        dialog.findViewById<TextView>(R.id.txvTitleField).text = spTitle
+        dialog.findViewById<TextView>(R.id.txvTitle).text = "Update " + spTitle
+        dialog.findViewById<TextView>(R.id.txvDateField).text = formatDate(spDateTime)
+        dialog.findViewById<TextView>(R.id.txvTimeField).text = formatTime(spDateTime)
+        dialog.findViewById<TextView>(R.id.txvLocationField).text = spLocation
+        dialog.findViewById<TextView>(R.id.txvDescField).text = spDesc
+
+        dialog.show()
     }
 }
