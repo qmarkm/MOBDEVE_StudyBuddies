0c40fd084a22ca4ca492bbef7736f8bb84c5f3c5
mattescober <145801129+matt3scober@users.noreply.github.com>
2024-12-09 12:25:58 +0800

    Implemented LocalDB Task in mainActivity
    Bugs are in creation of tasks

--- app/src/main/java/com/mobdeve/s12/group10/mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group10/mco/MainActivity.kt
@@ -3,19 +3,24 @@ package com.mobdeve.s12.group10.mco
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobdeve.s12.group10.mco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var taskDatabase: TaskDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        // Initialize TaskDatabase
+        taskDatabase = TaskDatabase(applicationContext)
+
+        loadTasks()
 
         viewBinding.btnStudyPact.setOnClickListener {
             val intent = Intent(this, MainStudyPacts::class.java)
@@ -36,8 +41,6 @@ class MainActivity : AppCompatActivity() {
         viewBinding.rcvSP.layoutManager = LinearLayoutManager(this)
         viewBinding.rcvSP.visibility = View.GONE
 
-        viewBinding.rcvCalendar.adapter = TaskAdapter(this, this, DataGenerator.loadTasks())
-        viewBinding.rcvCalendar.layoutManager = LinearLayoutManager(this)
 
         viewBinding.btnToggleSP.setOnClickListener {
             viewBinding.rcvSP.visibility = View.VISIBLE
@@ -48,5 +51,11 @@ class MainActivity : AppCompatActivity() {
             viewBinding.rcvCalendar.visibility = View.VISIBLE
             viewBinding.rcvSP.visibility = View.GONE
         }
+    }
+
+    private fun loadTasks() {
+        val tasks = taskDatabase.getAllTasks()
+        viewBinding.rcvCalendar.adapter = TaskAdapter(this, tasks)
+        viewBinding.rcvCalendar.layoutManager = LinearLayoutManager(this)
     }
 }
