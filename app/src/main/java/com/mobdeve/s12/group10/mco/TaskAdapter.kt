3f349cd7b9d8e5692b58c7a06f77a3c45fe1e426
Matthew Escober <escobermatthew1029@gmail.com>
2024-12-09 12:06:15 +0800

    localDB implementation (bugs)
    :((((

--- app/src/main/java/com/mobdeve/s12/group10/mco/TaskAdapter.kt
+++ app/src/main/java/com/mobdeve/s12/group10/mco/TaskAdapter.kt
@@ -1,21 +1,24 @@
 package com.mobdeve.s12.group10.mco
 
-import android.content.Intent
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s12.group10.mco.databinding.DialogTaskDetailedBinding
 import com.mobdeve.s12.group10.mco.databinding.TaskSmallLayoutBinding
+import com.mobdeve.s12.group10.mco.Task
 
-class TaskAdapter(private val context : Context, private val fragmentActivity: FragmentActivity, private val tasks: ArrayList<Task>): RecyclerView.Adapter<TaskViewHolder>(), OnDatePass, OnTimePass {
+class TaskAdapter(
+    private val context: Activity,
+    private var tasks: ArrayList<Task> // Make tasks mutable
+) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
     private lateinit var dialogBinding: DialogTaskDetailedBinding
+    private val taskDatabase = TaskDatabase(context) // Access the TaskDatabase
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val taskViewBinding: TaskSmallLayoutBinding = TaskSmallLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -27,54 +30,73 @@ class TaskAdapter(private val context : Context, private val fragmentActivity: F
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.bindData(tasks.get(position))
+        holder.bindData(tasks[position])
 
-        holder.itemView.setOnClickListener{
-            showEditTaskDialog(context, tasks.get(position))
+        holder.itemView.setOnClickListener {
+            showEditTaskDialog(tasks[position], position)
         }
     }
 
-    private fun showEditTaskDialog(context: Context, task: Task) {
+    private fun showEditTaskDialog(task: Task, position: Int): Dialog {
         dialogBinding = DialogTaskDetailedBinding.inflate(LayoutInflater.from(context))
-        val dialog = AlertDialog.Builder(context)
-            .setView(dialogBinding.root)
-            .create()
+        val builder = AlertDialog.Builder(context)
+        val dialog = builder.setView(dialogBinding.root).create()
 
-        dialog.dismiss()
+        // Populate dialog with current task data
+        dialogBinding.editActivityTitleInput.setText(task.name)
+        dialogBinding.editActivityDateInput.setText(task.date)
+        dialogBinding.editActivityTimeInput.setText(task.time)
+        dialogBinding.editActivityDescriptionInput.setText(task.desc)
 
         dialogBinding.saveActivityButton.setOnClickListener {
+            val updatedTask = task.copy(
+                name = dialogBinding.editActivityTitleInput.text.toString(),
+                date = dialogBinding.editActivityDateInput.text.toString(),
+                time = dialogBinding.editActivityTimeInput.text.toString(),
+                desc = dialogBinding.editActivityDescriptionInput.text.toString()
+            )
+
+            // Update task in database
+            taskDatabase.updateTask(updatedTask)
+            tasks[position] = updatedTask // Update the local list
+            notifyItemChanged(position) // Notify adapter to refresh the item
             dialog.dismiss()
         }
 
         dialogBinding.deleteActivityButton.setOnClickListener {
-            //TODO: Delete
-            //TODO: Update RecyclerView to remove view
+            taskDatabase.deleteTask(task) // Pass the entire task object
+            tasks.removeAt(position) // Remove task from the list
+            notifyItemRemoved(position) // Notify adapter to remove item
             dialog.dismiss()
         }
 
-        dialogBinding.lyvEditDate.setOnClickListener{
+
+        dialogBinding.lyvEditDate.setOnClickListener {
             val newFragment = DatePickerFragment()
-            newFragment.show(fragmentActivity.supportFragmentManager, "datePicker")
+            newFragment.show((context as FragmentActivity).supportFragmentManager, "datePicker")
         }
 
-        dialogBinding.lyvEditTime.setOnClickListener{
+        dialogBinding.lyvEditTime.setOnClickListener {
             val newFragment = TimePickerFragment()
-            newFragment.show(fragmentActivity.supportFragmentManager, "timePicker")
+            newFragment.show((context as FragmentActivity).supportFragmentManager, "timePicker")
         }
-
-        dialogBinding.editActivityTitleInput.setText(task.name)
-        dialogBinding.editActivityDateInput.setText(FormatDateTime.formatDate(task.date))
-        dialogBinding.editActivityTimeInput.setText(FormatDateTime.formatTime(task.time))
-        dialogBinding.editActivityDescriptionInput.setText(task.desc)
 
         dialog.show()
+        return dialog
     }
 
-    override fun onDatePass(data: String) {
-        dialogBinding.editActivityDateInput.setText(data)
+    // Method to refresh tasks list
+    fun refreshTasks() {
+        tasks = ArrayList(taskDatabase.getAllTasks()) // Reload tasks from the database
+        notifyDataSetChanged() // Refresh the RecyclerView
     }
 
-    override fun onTimePass(data: String) {
-        dialogBinding.editActivityTimeInput.setText(data)
+    inner class TaskViewHolder(private var taskBinding: TaskSmallLayoutBinding) : RecyclerView.ViewHolder(taskBinding.root) {
+        fun bindData(task: Task) {
+            taskBinding.txvTaskTitle.text = task.name
+            taskBinding.txvTaskDate.text = FormatDateTime.formatDate(task.date)
+            taskBinding.txvTaskTime.text = FormatDateTime.formatTime(task.time)
+            taskBinding.txvShortDesc.text = task.desc
+        }
     }
 }
